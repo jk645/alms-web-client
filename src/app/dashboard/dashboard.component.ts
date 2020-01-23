e651951f7a60d572f3084f1b6b701056442084c3
Josiah Kruse <sik645@gmail.com>
2020-01-23 15:48:31 -0800

    Got login and simple dashboard working

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+import { LoginService } from '../login-service/login.service';
+import { User } from '../login-service/user';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  faSpinner = faSpinner;
+  fetchingCourses: boolean = false;
+  fetchingCoursesError: any = null;
 
-  constructor() { }
+  user: User = null;
+  courses: any[] = null;
+
+  constructor(
+    private loginService: LoginService,
+  ) { }
 
   ngOnInit() {
+    this.user = this.loginService.getUser();
+    this.refreshData();
+  }
+
+  refreshData() {
+    if (this.fetchingCourses) {
+      return;
+    }
+    this.fetchingCourses = true;
+    this.fetchingCoursesError = null;
+
+    this.courses = [
+      {name: 'Intensive Thinking Strategically in the Digital Age (MGMTMBA 200)'},
+      {name: 'Merage Consulting Projects (MGMTMBA 298)'},
+      {name: 'New Venture Management (MGMTMBA 213)'},
+      {name: 'Organizational Leadership for Management (MGMTMBA 202)'},
+      {name: 'Business Law (MGMTMBA 292)'},
+    ];
   }
 
 }
